1649cd2b5e86b1c9f3853911a235a5f2f75e8d81
Moneymanager88 <saifextra99@gmail.com>
2026-02-25 15:14:50 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
-
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("money-manager").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js"
+      ]);
+    })
+  );
+});
